6c56dcf486faecc44db0f120f55f76a361dba7c4
Olivera Radovanović <90214731+Olivera2708@users.noreply.github.com>
2024-05-23 21:53:45 +0200

    set to correct client-id

--- PKISecurity/src/main/java/com/example/pkisecurity/utils/ApplicationProperties.java
+++ PKISecurity/src/main/java/com/example/pkisecurity/utils/ApplicationProperties.java
@@ -1,5 +1,5 @@
 package com.example.pkisecurity.utils;
 
 public class ApplicationProperties {
-    public static String SECURITY_PATH = "C:/Users/Dell/Desktop/security/podaci/";
+    public static String SECURITY_PATH = "/Users/olivera/Documents/Bezbednost-tajne/";
 }
